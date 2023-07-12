027097f057b5885adcdaddad9d9bd37352645ed4
dabizha.vanya <dabizha.vanya@gmail.com>
2023-07-12 16:23:11 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+//
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+//
+import path from 'path';
+
+export default defineConfig({
+	server: {
+		port: 8080,
+	},
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@ui': path.resolve(__dirname, './src/shared/ui-kit'),
+      '@store': path.resolve(__dirname, './src/app/store/index.ts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@assets': path.resolve(__dirname, './src/shared/assets'),
+      '@vars': path.resolve(__dirname, './src/shared/styles/vars.scss'),
+    },
+  },
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    react()
+  ],
+})
